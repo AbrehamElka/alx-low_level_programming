cddac7558af5f6614628cfa104391e4e15c89c29
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-08 11:57:10 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
  * it will check if n is positive, negative or zero
  *
  * Return : 0 on success
- * /
-/* betty style doc for function main goes there */
-
+ */
+ 
 int main(void)
 {
 	int n;
