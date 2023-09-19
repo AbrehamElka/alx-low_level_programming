25d4f5971c63564689a73bb4ea489f5f31e3cfbf
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-19 11:43:05 -0700

    created print_array function

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - prints arrays elements
+ * @a: pointer to array
+ * n: num of arrayes to be printed
+ */
+void print_array(int *a, int n)
+{
+	for (int i = 0; i <= n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+
+	printf("\n");
+}
