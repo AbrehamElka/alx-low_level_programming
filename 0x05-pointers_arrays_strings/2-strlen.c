f4a98f5aad100d9db4e447b107f62c4db71c8208
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-19 09:01:17 -0700

    created function _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of the string
+ *
+ * @s: pointer to first value
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	c=0;
+
+	while(s[c] != '\0')
+	{
+		c++;
+	}
+}
