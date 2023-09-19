a3e3103d9f0ae9502d3bb91aaa9120bc98083bc0
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-19 10:16:16 -0700

    created reverse_string function

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.c"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to a string to be reversed
+ */
+void rev_string(char *s)
+{
+	int len, i, j;
+	char c;
+
+	i = 0;
+
+	while (s[i] != '\n')
+	{
+		i++;
+	}
+
+	len = i - 1;
+	j = 0;
+
+	while (len > j)
+	{
+		c = s[j];
+		s[j] = s[j - len];
+		s[len] = s[i];
+		j++;
+	}
+}
