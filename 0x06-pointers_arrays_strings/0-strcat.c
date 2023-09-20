75560a06143d9d1f0b55c43f01459f91e9cc3f0c
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-20 19:28:17 +0300

    created a file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcat - concats two strings
+ * @dest: pointer to first string
+ * @src: pointer to second string
+ * Return: return dest as a string
+ */
+char *_strcat(char *dest, char src *src)
+{
+	return (dest);
+}
