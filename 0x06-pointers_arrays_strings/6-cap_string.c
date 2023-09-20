125bf7811d3fd1a1786cdb9d9e6c64d02ac86e23
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-20 23:59:15 +0300

    created 6-string_toupper.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *n)
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (n[i + 1] == ' ')
+		if (n[i + 1] == ' ' || n[i + 1] == '\n')
 		{
 			n[i + 2] = n[i + 2] -32;
 			n = n + 2;
