20e408c751c023ae289f43d5003787a018a3fb91
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-20 21:11:37 +0300

    created 2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcpy - copies a string
+ * @dest: pointer to first string
+ * @src: pointer to string to be copied
+ * @n: number of string to cpy
+ *
+ * Return: returns the new string
+ */
+char *_strcpy(char *dest, char *src, int n)
+{
+	int lenS, lenD;
+
+	lenS = 0;
+
+	while (*(src + lenS) != '\0' && lenS < n - 1)
+	{
+		*(dest + lenS) = *(src + lenS); 
+		lenS++;
+	}
+
+	*(dest + lenS) = '\0';
+	return (dest);
+}
