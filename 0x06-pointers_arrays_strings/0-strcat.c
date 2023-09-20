9917040b03dc7c52dc536010d031edcb9b8c8015
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-20 19:38:06 +0300

    created srtcat function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,5 +8,37 @@
  */
 char *_strcat(char *dest, char src *src)
 {
+	int len, j, i, tot, m;
+
+	len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	
+	m = 0;
+	i = 0;
+	tot = j + len - 1;
+	while (i < tot)
+	{
+		if (i < len)
+		{
+			dest[i] = dest[i];
+		}
+		else
+		{
+			dest[i] = src[m];
+			m++;
+		}
+
+		i++;
+	}
 	return (dest);
 }
