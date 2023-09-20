090d40ae5f1643b234bd0825199fe993a9cd6935
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-20 22:23:31 +0300

    edited 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,28 @@
 #include "main.h"
+
 /**
  * _strcpy - copies a string
  * @dest: pointer to first string
  * @src: pointer to string to be copied
  * @n: number of string to cpy
- *
  * Return: returns the new string
  */
 char *_strncpy(char *dest, char *src, int n);
 {
 	int lenS;
 
-	lenS = 0;
-
-	for (lenS = 0; src[lenS] != '\0' && lenS < n; lenS++)
+	for (lenS = 0; src[lenS] != '\0'; lenS++)
 	{
-		dest[lenS] = src[lenS];
+		if (lenS < n)
+		{
+			dest[lenS] = src[lenS];
+		}
 	}
 
 	while (lenS < n)
 	{
 		dest[lenS++] = '\0';
 	}
+
 	return (dest);
 }
