8c0abe5a70a9293df151d1e25ba42840cbd803df
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-21 00:06:21 +0300

    created 6-string_toupper.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,15 +7,29 @@
  */
 char *cap_string(char *n)
 {
-	int i;
+	int i, cap;
 
+	cap = 32;
+
+	char separators[] = {',', ';', '.', '?', '"',
+			    '(', ')', '{', '}', ' ', '\n', '\t'};
+	
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (n[i + 1] == ' ' || n[i + 1] == '\n')
+		if (n[i] >= 'a' && n[i] <= 'z')
 		{
-			n[i + 2] = n[i + 2] -32;
-			n = n + 2;
-			continue;
+			n[i] = n[i] - cap;
+		}
+
+		cap = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (n[i] == separators[x])
+			{
+				cap = 32;
+				break;
+			}
 		}
 	}
 
