9bbe23029c757822c8f7316294c3b3a2efb07d1f
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-25 20:17:57 +0300

    task zero - memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,19 @@
 #include "main.h"
 
+/**
+ * _memset - a function that filles a memory
+ * @s: the poiter to string
+ * @b: pointer to the char
+ * @n: pointer to a number
+ */
 char *_memset(char *s, char b, unsigned int n);
 {
-	b = 'a';
-	n =  3;
-	return s;
+
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
