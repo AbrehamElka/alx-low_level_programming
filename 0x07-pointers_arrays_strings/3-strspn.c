6926688e8a560fe25ec9883ea06fc6714a298859
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-09-25 21:45:07 +0300

    task 2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,28 +8,17 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, k;
-	unsigned int n;
-	n = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	int i = 0, k = 0;
+	unsigned int n = 0;
+
+	for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
 	{
 		for (k = 0; accept[k] != '\0'; k++)
 		{
 			if (s[i] == accept[k])
-			{
-				(break);
-			}
-
-			if (accept[k + 1] == '\0')
-			{
-				n = (unsigned int) i;
-				return (n);
-			}
-
+				n++;
 		}
+		k = 0;
 	}
-
-	n = 0;
-
 	return (n);
 }
