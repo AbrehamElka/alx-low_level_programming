0b76ffc3caad3bb28c13e908f74fe9f15537425e
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-10-25 20:19:28 +0300

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,18 +3,18 @@
 /**
  * _puts_recursion: prints a string, followed by a new line.
  * @s: pointer to a char
- *
  */
 void _puts_recursion(char *s)
 {
+
 	_putchar(*s);
 
-	if (*(s + 1) != 0)
+	if (*s == 0)
 	{
-		_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 	else
 	{
-		_putchar('\n');	
+		_puts_recursion(s + 1);	
 	}
 }
