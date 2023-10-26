b5ab6e5dc134b9a1a35e858c9e785250bc5858cf
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-10-26 11:42:16 +0300

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,22 @@
 #include "main.c"
 
 
+
+/**
+ * _strlen- Returns Length of String
+ * @s: string
+ *
+ * Return: on success 1
+ */
+int _strlen(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen(s + 1));
+	}
+	return (0);
+}
+
 /**
  * is_palindrome: check if palindrom.
  * @s: string
@@ -11,7 +27,7 @@ int is_palindrome(char *s)
 {
 	int len, left, right;
 
-	len = _strlen_recursion(s);
+	len = _strlen(s);
 	left = 0;
 	right = len - 1;
 
