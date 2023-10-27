08d3f83a7e620be8e184abf1577bc3824fc7cf8e
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-10-27 15:26:38 +0300

    task 1 and 0

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,5 +1,28 @@
 #include "main.h"
 int _atoi(char *s)
 {
-	return (0);
+	unsigned int count = 0, size = 0, o = 0, pnt = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+		if (*(s + count) == '-')
+			pnt *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		o = o + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (o * pnt);
 }
