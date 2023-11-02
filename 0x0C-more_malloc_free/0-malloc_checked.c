c98b1b32474e64c9238ea76bcdf5c9e7531e5da5
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-11-02 13:44:04 +0300

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Write a function that allocates memory
+ * @b: size of memory
+ *
+ * Return: a pointer on success and 98 if it fails
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		*ptr = 98;
+		return (*ptr);
+	}
+	return (ptr);
+}
