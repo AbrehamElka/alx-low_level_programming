796cb425a637b3ee283932ffc6892dbecfda2869
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-11-02 15:10:05 +0300

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,22 +12,25 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int i;
-	
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+    	unsigned int i;
 
-	i = strlen(s2) - n;
-	if (i >= 0)
-	{
-		ptr = (char *) malloc(strlen(s1) + n + 1);
-	}
-	else
-		ptr = (char *) malloc(strlen(s1) + strlen(s2) + 1);
-	
-	if (ptr == NULL)
-		return (NULL);
-	return (ptr);
+    	if (s1 == NULL)
+        	s1 = "";
+    	if (s2 == NULL)
+        	s2 = "";
+
+    	i = strlen(s2) - n;
+    	if (i >= 0)
+    	{
+        	ptr = (char *)malloc(strlen(s1) + n + 1);
+    	}
+    	else
+        	ptr = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+
+    	if (ptr == NULL)
+        	return (NULL);
+
+    	strcpy(ptr, s1);
+    	strncat(ptr, s2, n);
+    	return (ptr);
 }
