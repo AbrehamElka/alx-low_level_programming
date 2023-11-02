29e2a0d1e3de5e347c62fb632c240d1b6ceb4e0e
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-11-02 14:36:29 +0300

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,14 +6,21 @@
  * @s1: first string
  * @s2: second string
  * @n: number of bytes
+ *
+ * Return: the pointer to the memory or NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr, i;
 	
 	i = sizeof(s2) - n;
+
+	if (s2 == NULL)
+		i = 0;
 	if (i >= 0)
+	{
 		ptr = malloc(sizeof(s1) + n);
+	}
 	else
 		ptr = malloc(sizeof(s1) + sizeof(s2));
 	if (ptr == NULL)
