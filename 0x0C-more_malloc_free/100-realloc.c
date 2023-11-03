52894e9b7852c7b247d4b2e2c636551a3f6b9725
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-11-03 14:33:37 +0300

    task 4 advanced

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,39 +1,44 @@
 #include "main.h"
+
+
 /**
- * my_realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated with malloc
- * @old_size: size in bytes of the allocated space for ptr
- * @new_size: new size in bytes of the memory block
- *
- * Return: a pointer to the newly allocated memory
- */
+* _realloc - reallocates a memory block
+* @ptr: pointer to the memory previously allocated with a call to malloc
+* @old_size: size of ptr
+* @new_size: size of the new memory to be allocated
+*
+* Return: pointer to the address of the new memory block
+*/
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	void *temp_block;
+	unsigned int i;
 
-	if (new_size == 0)
+	if (ptr == NULL)
+	{
+		temp_block = malloc(new_size);
+		return (temp_block);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+
+	else if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
-	{
-		new_ptr = malloc(newsize);
-		return (new_ptr);
-	}
-	if (old_size == new_size)
-		return (ptr);
-
-	new_ptr = malloc(new_size);
-
-	if (new_ptr == NULL)
-		return (NULL);
-
-	if (new_size > old_size)
-		memcpy(new_ptr, ptr, old_size);
 	else
-		memcpy(new_ptr, ptr, new_size);
-
-	free(ptr);
-	return (new_ptr);
+	{
+		temp_block = malloc(new_size);
+		if (temp_block != NULL)
+		{
+			for (i = 0; i < min(old_size, new_size); i++)
+				*((char *)temp_block + i) = *((char *) ptr + i);
+			free(ptr);
+			return (temp_block);
+		}
+		else
+			return (NULL);
+	}
 }
