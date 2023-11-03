a5503481efefe7860768369166cfd79051cad0b5
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-11-03 14:29:46 +0300

    task 4 advanced

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,26 +9,31 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *new_ptr;
+
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	if (ptr == NULL)
-		return (malloc(new_size));
-	if (new_size == old_size)
+	{
+		new_ptr = malloc(newsize);
+		return (new_ptr);
+	}
+	if (old_size == new_size)
 		return (ptr);
 
-	void *new_ptr = malloc(new_size);
+	new_ptr = malloc(new_size);
+
 	if (new_ptr == NULL)
 		return (NULL);
+
 	if (new_size > old_size)
 		memcpy(new_ptr, ptr, old_size);
 	else
 		memcpy(new_ptr, ptr, new_size);
 
 	free(ptr);
-
 	return (new_ptr);
 }
