2a261de74f01f1b87d0a315231838bc1289b892b
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2023-11-08 20:18:52 +0300

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,5 +10,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 	for (i = 0; i< size; i++)
-		action(i);
+		action(array[i]);
 }
