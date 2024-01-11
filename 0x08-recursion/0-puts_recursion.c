8167a008ac3a92cacff74fe80269852337cb8446
Abreham <abrehamdessalegn02@gmail.com>
2024-01-11 15:34:26 +0000

    use only pointers and changend the if statments

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,14 @@
 */
 void _puts_recursion(char *s)
 {
-	if (s[0] == '\0')
+	if (*s)
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
-		_putchar(s[0]);
-		_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 
 	_putchar('\n');
