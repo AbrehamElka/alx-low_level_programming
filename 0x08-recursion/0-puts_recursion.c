fc8e1bf815207bbc0d61ea88255538fc6d17fd73
Abreham <abrehamdessalegn02@gmail.com>
2024-01-11 15:30:08 +0000

    removed a loop

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,15 @@
 */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	if (s[0] == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar('\n');
 	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+
 	_putchar('\n');
 }
