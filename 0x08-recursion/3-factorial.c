9cd0ed969fc68345a2e6d659034784969bf6a522
Abreham <abrehamdessalegn02@gmail.com>
2024-01-11 16:25:23 +0000

    created a function that does a factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* factorial: gives the factorial of a number.
+* @n: the number to be factorised.
+*
+* Return: the factorial of a number(int).
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
