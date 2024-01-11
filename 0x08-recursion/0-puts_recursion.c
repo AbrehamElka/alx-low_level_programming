26a0bc33b71edff951a6763ee28d893069cc3c0d
Abreham <abrehamdessalegn02@gmail.com>
2024-01-11 15:18:32 +0000

    edited for betty style code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,14 @@
 #include "main.h"
-
+/**
+* _puts_recursion - prints string recursively.
+* @s: stores the string to be printed.
+*
+* Return: void.
+*/
 void _puts_recursion(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		_putchar(s[i]);
