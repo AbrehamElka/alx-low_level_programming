abdae6af5bbf6d79f6b470ac5fa20f8c4143d8d0
Abreham <abrehamdessalegn02@gmail.com>
2024-01-17 04:28:30 +0000

    a function that duplicates a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strdup - duplicates a string to an array.
+* @str: the string to be duplicated.
+*
+* Return: char array with value of str.
+*/
+char *_strdup(char *str)
+{
+	unsigned int size, i;
+	char *s;
+
+	size = 0;
+	for ( ; str[size] != '\0'; )
+	{
+		size++;
+	}
+	s = malloc((sizeof(char) * size) + 1);
+	i = 0;
+	for ( ; i <= size; )
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[size + 1] = '\0';
+	return (s);
+}
