bbe8bde1e7e2e48930fc30d8de0c1dd22cf023dd
Abreham <abrehamdessalegn02@gmail.com>
2024-01-17 13:01:31 +0000

    intro to argc and argv.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main -  the main function.
+* @argc: num of arguments.
+* @argv: the arguments.
+*
+* Return: 0 on succes.
+*/
+int main(int argc, int *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
