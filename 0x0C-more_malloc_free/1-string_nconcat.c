de72baed9a8eea370413837fa1edc6adcd378dd0
Abreham <abrehamdessalegn02@gmail.com>
2024-01-18 15:59:38 +0000

    functionto concat n bytes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		tot = len1 + n;
 
-	str = malloc(sizeof(char) * tot);
+	str = malloc(sizeof(char) * tot + 1);
 	if (str == NULL)
 		return (NULL);
 
