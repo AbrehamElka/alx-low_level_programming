06be824681d8db59159af5116b3c3a92c96fad80
Abreham <abrehamdessalegn02@gmail.com>
2024-01-18 15:45:42 +0000

    functionto concat n bytes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,30 +23,42 @@ unsigned int strlen_(char *str)
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, k, m, len1, len2;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *str;
 
-	len1 = strlen_(s1);
-	len2 = strlen_(s2);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (len2 <= n)
-		i = len2;
-	else
-		i = n;
-	i = i + len1 + 1;
-	str = malloc(i * sizeof(char));
 
+	while (s1[i])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
 
-	for (k = 0; k < len1; k++)
-		str[k] = s1[k];
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
 
-	for (m = 0; (m + k) < i; m++)
-		str[k + m] = s2[m];
-	str[i] = '\0';
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
 	return (str);
 }
