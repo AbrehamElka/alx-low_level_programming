42f231aed0a7ddd035e38069a30a8cf80f26cbea
Abreham <abrehamdessalegn02@gmail.com>
2024-01-18 09:37:20 +0000

    memory allocator function

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <ctype.h>
 /**
 * malloc_checked - allocates memory with size b.
 * @b: size of the memory.
@@ -11,7 +12,7 @@ void *malloc_checked(unsigned int b)
 	void *i;
 
 	i = malloc(b);
-	if (i == NULL)
+	if (i == NULL || !((isdigit(b))) || b <= 0)
 		return ((int *) 98);
 	return (i);
 }
