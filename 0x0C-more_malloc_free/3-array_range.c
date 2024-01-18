4f39d3b2711f30c04f8a2ab61e866ff8d01865ae
Abreham <abrehamdessalegn02@gmail.com>
2024-01-18 16:59:58 +0000

    function to allocate: int array for with in the range of two nums

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* counter - countes the amount of numbers between two ranges.
+* @min: first num.
+* @max: second num.
+*
+* Return: the amount of num.
+*/
+int counter(int min, int max)
+{
+	int i;
+
+	for (i = 0; min <= max; i++)
+		min++;
+	return (i);
+}
+/**
+* array_range - creates malloc between two num ranges.
+* @min: first num.
+* @max: second num.
+*
+* Return: a newly allocated memory for the range.
+*/
+int *array_range(int min, int max)
+{
+	int i, k, *ptr;
+
+	if (min > max)
+		return (NULL);
+	i = counter(min, max);
+	ptr = malloc(i * sizeof(int));
+	for (k = 0; k < i; k++)
+	{
+		ptr[k] = min;
+		min++;
+	}
+	return (ptr);
+}
