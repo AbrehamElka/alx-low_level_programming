baa75a88ce2453338b271e0af48131e40b1ac37d
Abreham <abrehamdessalegn02@gmail.com>
2024-02-17 16:53:06 +0000

    working with structs

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+/**
+* init_dog - initializes a varable for dog
+* @d: the dog.
+* @name: the name of the dog.
+* @age: age of the dog.
+* @owner: owner of the dog.
+*
+* Return: Nothing.
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
