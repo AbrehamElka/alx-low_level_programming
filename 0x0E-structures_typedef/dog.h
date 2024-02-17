aedf5448c6971b8a43d8add0562ab3630287b3c5
Abreham <abrehamdessalegn02@gmail.com>
2024-02-17 16:43:19 +0000

    begging for struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_FILE
+#define DOG_FILE
+/**
+* dog_t - an alias for struct dog.
+*/
+typedef struct dog dog_t;
+/**
+* struct dog - stores info about a dog.
+* @name: the name of the dog.
+* @age: age of the dog.
+* @owner: the owner of the dog.
+*
+* Description: struct called "dog", that stores
+* name, age and owner of a dog.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
