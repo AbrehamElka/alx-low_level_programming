2a373efd5f670176c970f30f092abbaa4991d6ce
Abreham Elka <abrehamdessalegn02@gmail.com>
2024-03-28 22:52:33 +0300

    adding a documentaion for file 0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _isupper - checkes for a char is upper or not.
+ * @c: the char to be checked.
+ *
+ * Return: 1 if succes and 0 if not
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
