2b05c1ec93ac262a3be5f39b1b5a91fba673d288
Abreham Elka <abrehamdessalegn02@gmail.com>
2024-03-29 18:47:50 +0300

    accessing shaered library in c

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * add - returns the addition of two nums.
+ * @a: first num.
+ * @b: second num.
+ *
+ * Return: the sum of a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - returns the subtraction of two nums.
+ * @a: first num.
+ * @b: second num.
+ *
+ * Return: the sub of a and b.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - returns the multiplication of two nums.
+ * @a: first num.
+ * @b: second num.
+ *
+ * Return: the mul of a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - returns the divition of two nums.
+ * @a: first num.
+ * @b: second num.
+ *
+ * Return: the div of a and b.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - returns the modules of two nums.
+ * @a: first num.
+ * @b: second num.
+ *
+ * Return: the module of a and b.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
