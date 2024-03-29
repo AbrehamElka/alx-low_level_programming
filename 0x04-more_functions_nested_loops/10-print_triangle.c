e5a8eee91d92858cea7daa7e46b0321c3c879fac
Abreham Elka <abrehamdessalegn02@gmail.com>
2024-03-29 09:05:22 +0300

    triangle printer

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle.
+ * @size: the base and the hight of the triangle
+ *
+ * Return: Nothing.
+ */
+void print_triangle(int size)
+{
+	int i, j, w;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i; j >= 0; j--)
+		{
+			_putchar(' ');
+		}
+		for (w = i; w > 0; w--)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
